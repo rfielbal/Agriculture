b628985200d3aabd4d1fcc98c827f472615aa0cf
OcTotoSama <thom.dom24@gail.com>
2026-03-13 11:10:18 +0100

    creation engrais/ une autre

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -15,6 +15,12 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Form\ParcelleType;
 use App\Entity\Parcelle;
 
+use App\Entity\ElementChimique;
+use App\Form\ElementChimiqueType;
+
+use App\Entity\Engrais;
+use App\Form\EngraisType;
+
 
 class BaseController extends AbstractController
 {
@@ -57,12 +63,54 @@ class BaseController extends AbstractController
 					$em->persist($parcelle);	
 					$em->flush();	
 					$this->addFlash('notice','Message envoyé'); 
-					return $this->redirectToRoute('app_contact');
+					return $this->redirectToRoute('app_parcelle');
 				}
 			}
 			return $this->render('base/parcelle.html.twig', [
 			'form' => $form->createView()
 		]);
 	}
+
+
+	#[Route('/Element-Chimique', name: 'app_elementChimique')]
+		public function ElementChimique(Request $request, EntityManagerInterface $em): Response
+		{
+			$elementChimique = new ElementChimique();
+			$form = $this->createForm(ElementChimiqueType::class, $elementChimique);
+			if($request->isMethod('POST')){
+				$form->handleRequest($request);
+				if ($form->isSubmitted()&&$form->isValid()){
+					$em->persist($elementChimique);	
+					$em->flush();	
+					$this->addFlash('notice','Message envoyé'); 
+					return $this->redirectToRoute('app_elementChimique');
+				}
+			}
+			return $this->render('base/elementChimique.html.twig', [
+			'form' => $form->createView()
+		]);
+	}
+
+	#[Route('/Engrais', name: 'app_engrais')]
+		public function Engrais(Request $request, EntityManagerInterface $em): Response
+		{
+			$engrais = new Engrais();
+			$form = $this->createForm(EngraisType::class, $engrais);
+			if($request->isMethod('POST')){
+				$form->handleRequest($request);
+				if ($form->isSubmitted()&&$form->isValid()){
+					$em->persist($engrais);	
+					$em->flush();	
+					$this->addFlash('notice','Message envoyé'); 
+					return $this->redirectToRoute('app_engrais');
+				}
+			}
+			return $this->render('base/engrais.html.twig', [
+			'form' => $form->createView()
+		]);
+	}
+
+
+
 }
 
