830e4649e878e43ea9703d249f4db3ea7569c0cb
darkskullz9 <darxkullz9@gmail.com>
2026-03-13 09:56:32 +0100

    Création formulaire date

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -2,16 +2,33 @@
 
 namespace App\Controller;
 
+use App\Form\DateType;
+use App\Entity\Date;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BaseController extends AbstractController
 {
-	#[Route('/', name: 'app_accueil')]
-	public function index(): Response
+	#[Route('/date', name: 'app_date')]
+	public function date(Request $request, EntityManagerInterface $em): Response
 	{
+		$date = new Date();
+		$form = $this->createForm(DateType::class,$date);
+
+		if($request->isMethod('POST')){
+			$form->handleRequest($request);
+			if($form->isSubmitted()&&$form->isValid()){
+				$em->persist($date);
+				$em->flush();
+				$this->addFlash('notice','Formulaire envoyé');
+				return $this->redirectToRoute('app_date');
+			}
+		};
 		return $this->render('base/index.html.twig', [
+			'form' => $form->createView
 		]);
 	}
 }
